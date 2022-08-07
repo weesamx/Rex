340217c573b2c7f3f6e3d8442ec66b1c27e5bda6
weesamx <weesam77@gmail.com>
2022-08-07 18:20:17 +0800

    Initial Commit

---
+++ Shared/RexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RexApp.swift
+//  Shared
+//
+//  Created by Sam on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
